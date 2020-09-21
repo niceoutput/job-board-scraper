e0f173ba3db5d0a5b1c6fcbaf21d2b5f2a43117e
mario <mario@uniplaces.com>
2020-09-21 18:32:29 +0100

    hide credentials

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ const doc = new GoogleSpreadsheet(
   console.log(doc.title);
   await doc.updateProperties({ title: "renamed doc" });
 
-  // const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-  // console.log(sheet.title);
-  // console.log(sheet.rowCount);
-
-  // // adding / removing sheets
-  // const newSheet = await doc.addSheet({ title: "hot new sheet!" });
-  // await newSheet.delete();
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+  await sheet.addRows([
+    { title: "Software Engineer", location: "NY" },
+    { title: "Designer", location: "SF" },
+  ]);
 })();
